c114c9c80f708be8b5912a5ce5d0a5f0229b7f93
daleran <daleran@gmail.com>
2019-04-16 19:27:15 +0900

    Reogranized Project, added MapData and Tests

---
+++ AntMapGenerator.Tests/MapDataTests.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using AntMapGenerator;
+
+
+namespace AntMapGenerator.Tests
+{
+    public class MapDataTests
+    {
+        MapData data;
+
+        [SetUp]
+        public void Setup()
+        {
+            data = new MapData(new string[] { "Test1", "Test2" }, 10, 10);
+        }
+
+        [Test]
+        [TestCase(-1, 2, ExpectedResult = false)]
+        [TestCase(3, -1, ExpectedResult = false)]
+        [TestCase(10, 2, ExpectedResult = false)]
+        [TestCase(2, 10, ExpectedResult = false)]
+        [TestCase(0, 0, ExpectedResult = true)]
+        [TestCase(9, 9, ExpectedResult = true)]
+        public bool CheckBoundsTest(int x, int y)
+        {
+            return data.CheckBounds(x, y);
+        }
+
+        [Test]
+        [TestCase("BadName", ExpectedResult = -1)]
+        [TestCase("test1", ExpectedResult = -1)]
+        [TestCase("Test1", ExpectedResult = 0)]
+        [TestCase("Test2", ExpectedResult = 1)]
+        public int LayerNameToIDTest(string layer)
+        {
+            return data.LayerID(layer);
+        }
+
+        [Test]
+        [TestCase(5,5,0, 2, ExpectedResult = 2)]
+        [TestCase(0, 0, 0, 2, ExpectedResult = 2)]
+        [TestCase(9, 5, 0, 2, ExpectedResult = 2)]
+        [TestCase(5, 9, 0, 2, ExpectedResult = 2)]
+        public int SetAndRetrieveValues(int x, int y, int z, int value)
+        {
+            Vector3Int coord = new Vector3Int(x, y, z);
+            data[coord] = value;
+            return data[coord];
+        }
+
+        [Test]
+        [TestCase("Test1", 5, 0, 2, ExpectedResult = 2)]
+        [TestCase("Test2", 2, 3, 2, ExpectedResult = 2)]
+        public int SetAndRetrieveValuesByName(string layer, int x, int y, int value)
+        {
+            Vector2Int coord = new Vector2Int(x, y);
+            data[layer, coord] = value;
+            return data[layer,coord];
+        }
+
+        [Test]
+        public void IndexOffMapBoundsVector3()
+        {
+            Vector3Int coord = new Vector3Int(14,3,0);
+            Assert.That(() => data[coord] = 5, Throws.TypeOf<System.IndexOutOfRangeException>());
+        }
+
+        [Test]
+        public void IndexWrongLayerVector3()
+        {
+            Vector3Int coord = new Vector3Int(9, 3, 3);
+            Assert.That(() => data[coord] = 692, Throws.TypeOf<System.IndexOutOfRangeException>());
+        }
+
+        [Test]
+        public void IndexOffMapBoundsByVector2()
+        {
+            Vector2Int coord = new Vector2Int(3, 11);
+            Assert.That(() => data["Test1",coord] = -457, Throws.TypeOf<System.IndexOutOfRangeException>());
+        }
+
+        [Test]
+        public void IndexWithWrongLayerName()
+        {
+            Vector2Int coord = new Vector2Int(5, 5);
+            Assert.That(() => data["test2",coord] = -5, Throws.TypeOf<System.IndexOutOfRangeException>());
+        }
+
+    }
+}
---
+++ AntMapGenerator/MapData.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AntMapGenerator
+{
+    [System.Serializable]
+    public class MapData : IGrid<int>
+    {
+        readonly string[] layerNames;
+        readonly int[,,] data;
+        readonly int sizeX;
+        readonly int sizeY;
+
+        public int this[Vector3Int coord]
+        {
+            get { return data[coord.x, coord.y, coord.z]; }
+            set { data[coord.x, coord.y, coord.z] = value; }
+        }
+
+        public int this[string layer, Vector2Int coord]
+        {
+            get { return data[coord.x, coord.y, LayerID(layer)]; }
+            set { data[coord.x, coord.y, LayerID(layer)] = value; }
+        }
+
+        public MapData(string[] layerNames, int sizeX, int sizeY)
+        {
+            this.layerNames = layerNames;
+            this.sizeX = sizeX;
+            this.sizeY = sizeY;
+
+            data = new int[sizeX, sizeY, layerNames.Length];
+        }
+
+        public string LayerName (int id)
+        {
+            return layerNames[id];
+        }
+
+        public int LayerID(string name)
+        {
+            for (int i = 0; i < layerNames.Length; i++)
+            {
+                if (layerNames[i] == name)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public bool CheckBounds (int x, int y)
+        {
+            if (x < 0 || x >= sizeX || y < 0 || y >= sizeY)
+                return false;
+
+            return true;
+        }
+
+    }
+
+}
